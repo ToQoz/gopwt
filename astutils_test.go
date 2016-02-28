61855b05241fff2f3cc914d3ab59cfa1a198cfa7
Takatoshi Matsumoto <toqoz403@gmail.com>
2016-02-28 17:28:25 +0900

    test(gopwt): add tests for createUntypedExprFromBinaryExpr

--- astutils_test.go
+++ astutils_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/ToQoz/gopwt/assert"
 	"go/ast"
 	"go/parser"
 	"testing"
@@ -20,4 +21,73 @@ func TestIsAssert_Regression(t *testing.T) {
 
 	// Check no panic on isAssert when *ast.SelectorExpr.X is not *ast.Ident
 	isAssert(assertImportIdent, exprs.(*ast.CallExpr))
+}
+
+func TestCreateUntypedExprFromBinaryExpr(t *testing.T) {
+	var f *ast.CallExpr
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("1 + 2").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpADD")
+	assert.OK(t, f.Args[0].(*ast.BasicLit).Value == "1")
+	assert.OK(t, f.Args[1].(*ast.BasicLit).Value == "2")
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("1 - 2").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpSUB")
+	assert.OK(t, f.Args[0].(*ast.BasicLit).Value == "1")
+	assert.OK(t, f.Args[1].(*ast.BasicLit).Value == "2")
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("1 * 2").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpMUL")
+	assert.OK(t, f.Args[0].(*ast.BasicLit).Value == "1")
+	assert.OK(t, f.Args[1].(*ast.BasicLit).Value == "2")
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("1 / 2").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpQUO")
+	assert.OK(t, f.Args[0].(*ast.BasicLit).Value == "1")
+	assert.OK(t, f.Args[1].(*ast.BasicLit).Value == "2")
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("1 % 2").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpREM")
+	assert.OK(t, f.Args[0].(*ast.BasicLit).Value == "1")
+	assert.OK(t, f.Args[1].(*ast.BasicLit).Value == "2")
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("1 & 2").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpAND")
+	assert.OK(t, f.Args[0].(*ast.BasicLit).Value == "1")
+	assert.OK(t, f.Args[1].(*ast.BasicLit).Value == "2")
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("1 | 2").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpOR")
+	assert.OK(t, f.Args[0].(*ast.BasicLit).Value == "1")
+	assert.OK(t, f.Args[1].(*ast.BasicLit).Value == "2")
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("1 ^ 2").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpXOR")
+	assert.OK(t, f.Args[0].(*ast.BasicLit).Value == "1")
+	assert.OK(t, f.Args[1].(*ast.BasicLit).Value == "2")
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("1 &^ 2").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpANDNOT")
+	assert.OK(t, f.Args[0].(*ast.BasicLit).Value == "1")
+	assert.OK(t, f.Args[1].(*ast.BasicLit).Value == "2")
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("1 << 2").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpSHL")
+	assert.OK(t, f.Args[0].(*ast.BasicLit).Value == "1")
+	assert.OK(t, f.Args[1].(*ast.BasicLit).Value == "2")
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("1 >> 2").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpSHR")
+	assert.OK(t, f.Args[0].(*ast.BasicLit).Value == "1")
+	assert.OK(t, f.Args[1].(*ast.BasicLit).Value == "2")
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("true && true").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpLAND")
+	assert.OK(t, f.Args[0].(*ast.Ident).Name == "true")
+	assert.OK(t, f.Args[1].(*ast.Ident).Name == "true")
+
+	f = createUntypedExprFromBinaryExpr(mustParseExpr("false || false").(*ast.BinaryExpr)).(*ast.CallExpr)
+	assert.OK(t, f.Fun.(*ast.SelectorExpr).Sel.Name == "OpLOR")
+	assert.OK(t, f.Args[0].(*ast.Ident).Name == "false")
+	assert.OK(t, f.Args[1].(*ast.Ident).Name == "false")
 }
