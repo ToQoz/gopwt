bb649fd7eb09da617341d9f23fdb96067bd4f4cf
Takatoshi Matsumoto <toqoz403@gmail.com>
2016-02-28 17:03:20 +0900

    test(gopwt): add tests to isGoFile2 and isTestGoFile

--- utils_test.go
+++ utils_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/ToQoz/gopwt/assert"
 	"testing"
 )
 
@@ -25,4 +26,25 @@ func TestMust(t *testing.T) {
 
 		must(nil)
 	}()
+}
+
+func TestIsGoFile2(t *testing.T) {
+	assert.OK(t, isGoFile2("a.go") == true, "a.go is go file")
+	assert.OK(t, isGoFile2("a_test.go") == true, "a_test.go is go file")
+
+	assert.OK(t, isGoFile2(".a.go") == false, ".a.go is not go file")
+	assert.OK(t, isGoFile2("_a.go") == false, "_a.go is not go file")
+	assert.OK(t, isGoFile2("a.goki") == false, "a.goki is not go file")
+	assert.OK(t, isGoFile2("a") == false, "a is not go file")
+}
+
+func TestIsTestGoFile(t *testing.T) {
+	assert.OK(t, isTestGoFile("a_test.go") == true, "a_test.go is test go file")
+
+	assert.OK(t, isTestGoFile("a.go") == false, "a.go is test go file")
+	assert.OK(t, isGoFile2(".a_test.go") == false, ".a_test.go is not go file")
+	assert.OK(t, isGoFile2("_a_test.go") == false, "_a_test.go is not go file")
+	assert.OK(t, isTestGoFile("_a.go") == false, "_a.go is not test go file")
+	assert.OK(t, isTestGoFile("a.goki") == false, "a.goki is not test go file")
+	assert.OK(t, isTestGoFile("a") == false, "a is not test go file")
 }
